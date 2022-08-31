acdbaa8229501704c21e1bf081c2f10253ac57ab
Beastmode99789 <83955253+Beastmode99789@users.noreply.github.com>
2022-08-31 09:44:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 # 2020
-# The Raven-Storm Toolkit was programmed and developed by Taguar258.
+# The Raven-Storm Toolkit was programmed and developed by Taguar258 and revised by Qu.
 # The Raven-Storm Toolkit is published under the MIT Licence.
 # The Raven-Storm Toolkit is based on the CLIF-Framework.
-# The CLIF-Framework is programmed and developed by Taguar258.
+# The CLIF-Framework is programmed and developed by Taguar258 and revised by Qu.
 # The CLIF-Framework is published under the MIT Licence.
 
 # This script is a shortcut for everyone who does not want to install Raven-Storm to the bin path.
